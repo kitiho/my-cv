34c9d9f1145d9b8fbe36149d1a7770781d0858d1
enoshima7 <duanjipeng0328@gmail.com>
2020-03-02 23:56:46 +0800

    首页文字轮播

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,7 @@
 <template>
   <div class="home bg-blue w-100">
     <Toggle-button class="toggle cursor" @toggleNav="toggleNav" />
-    <div class="homepage text-white p-5 d-flex flex-column jc-around ">
-      <div class="homepage-title">
-        <span class="fs-lg fs-bold">Welcome to my Small Base</span>
-      </div>
-      <div class="homepage-body fs-xs fs-light">
-        <span>{{ sentences[0] }}</span>
-      </div>
-      <div class="homepage-foot d-flex ai-center">
-        <span class="fs-sm fs-bold">SEE WORKS</span>
-        <div
-          class="homepage-arrow  ml-4 arrow bg-blue d-flex ai-center jc-center bg-white"
-        >
-          <img width="20" src="../assets/arrow-blue.png" alt="" />
-        </div>
-      </div>
-      <div class="homepage-bg">
-        <img src="../assets/homepage.svg" width="210" alt="" />
-      </div>
-    </div>
-
+    <HomePage />
     <transition name="Nav">
       <Nav @closeNav="showNav = false" v-show="showNav" class="nav w-100" />
     </transition>
@@ -30,10 +11,12 @@
 <script>
 import Nav from "./Nav";
 import ToggleButton from "./ToggleButton";
+import HomePage from "./HomePage";
 export default {
   components: {
     Nav,
-    ToggleButton
+    ToggleButton,
+    HomePage
   },
   methods: {
     toggleNav() {
@@ -42,10 +25,7 @@ export default {
   },
   data() {
     return {
-      showNav: false,
-      sentences: [
-        "To dream the impossible dream.To fight the unbeatable foe.To bear with unbearable sorrow.To run where the brave dare not go."
-      ]
+      showNav: false
     };
   }
 };
@@ -53,7 +33,7 @@ export default {
 <style lang="scss" scoped>
 .toggle {
   width: 70px;
-  height:60px;
+  height: 60px;
   position: absolute;
   top: -20px;
   z-index: 999;
@@ -62,61 +42,5 @@ export default {
   position: absolute;
   top: 0;
   z-index: 100;
-}
-.homepage {
-  .homepage-bg {
-    position: absolute;
-    top: 130px;
-    right: 0px;
-    z-index: 1;
-  }
-  min-height: 100vh;
-  .homepage-title {
-    position: relative;
-    z-index: 2;
-    width: 9rem;
-    line-height: 2rem;
-  }
-  .homepage-body {
-    position: relative;
-    z-index: 2;
-    width: 9rem;
-    line-height: 1.3rem;
-  }
-  .homepage-foot {
-    .homepage-arrow {
-      img {
-        animation: move 2s infinite ease;
-      }
-      @keyframes move {
-        0% {
-          transform: translateY(0);
-        }
-        10% {
-          transform: translateY(-2px);
-        }
-        50% {
-          transform: translateY(5px);
-        }
-        90% {
-          transform: translateY(-2px);
-        }
-        100% {
-          transform: translateY(0px);
-        }
-      }
-    }
-  }
-}
-.Nav-enter-active {
-  transition: all 0.8s ease;
-}
-.Nav-leave-active {
-  transition: all 0.8s ease;
-}
-.Nav-enter, .Nav-leave-to
-/* .slide-fade-leave-active for below version 2.1.8 */ {
-  transform: translateX(-2rem);
-  opacity: 0;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="homepage text-white p-5 d-flex flex-column jc-around ">
+    <div class="homepage-title">
+      <span class="fs-lg fs-bold">Welcome </span>
+      <span class="fs-lg fs-bold">to my </span>
+      <span class="fs-lg fs-bold">Small Base</span>
+    </div>
+    <div class="homepage-body fs-xs fs-light d-flex">
+      <transition name="sentence" mode="out-in">
+        <span :key="index">
+          {{ sentences[index] }}
+        </span>
+      </transition>
+    </div>
+    <div class="homepage-foot d-flex ai-center">
+      <span class="fs-sm fs-bold">SEE WORKS</span>
+      <div
+        class="homepage-arrow  ml-4 arrow bg-blue d-flex ai-center jc-center bg-white"
+      >
+        <img width="20" src="../assets/arrow-blue.png" alt="" />
+      </div>
+    </div>
+    <div class="homepage-bg">
+      <img src="../assets/homepage.svg" width="210" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      index: 0,
+      sentenceActiveID: 0,
+      sentences: [
+        "热爱可抵岁月漫长。",
+        "第一次看到宇宙，是和你四目相对的时候。",
+        "「今でもあなたは私の光。」",
+        "就算你和我，还有整个愚蠢的世界，都将变成粉碎，我依然爱你。",
+        "行至朝雾里，坠入暮云间。"
+      ]
+    };
+  },
+  methods: {
+    refreshIndex() {
+      if (this.index + 1 == this.sentences.length) {
+        this.index = 0;
+      } else {
+        this.index = this.index + 1;
+      }
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.refreshIndex();
+    }, 5000);
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "../assets/scss/style.scss";
+.homepage {
+  .homepage-bg {
+    position: absolute;
+    top: 130px;
+    right: 0px;
+    z-index: 1;
+  }
+  min-height: 100vh;
+  .homepage-title {
+    position: relative;
+    z-index: 2;
+    width: 9rem;
+    line-height: 2rem;
+  }
+  .homepage-body {
+    height: 70px;
+    position: relative;
+    z-index: 2;
+    width: 9rem;
+    line-height: 1.3rem;
+  }
+  .homepage-foot {
+    .homepage-arrow {
+      img {
+        animation: move 2s infinite ease;
+      }
+      @keyframes move {
+        0% {
+          transform: translateY(0);
+        }
+        10% {
+          transform: translateY(-2px);
+        }
+        50% {
+          transform: translateY(5px);
+        }
+        90% {
+          transform: translateY(-2px);
+        }
+        100% {
+          transform: translateY(0px);
+        }
+      }
+    }
+  }
+}
+</style>
